a69b31da3456ccd7b78cd429b611e35327db7a70
Adhii04 <adhiishere04@gmail.com>
2026-01-19 14:15:42 +0530

    Fix Google OAuth flow

--- backend/app/google_auth.py
+++ backend/app/google_auth.py
@@ -11,6 +11,7 @@ settings = get_settings()
 
 # OAuth 2.0 scopes
 SCOPES = [
+    'openid',
     'https://www.googleapis.com/auth/userinfo.email',
     'https://www.googleapis.com/auth/userinfo.profile',
     'https://www.googleapis.com/auth/gmail.readonly',
